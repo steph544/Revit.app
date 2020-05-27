d60010fd101545d83afbead6e9b77576f79c1917
github username <github email address>
2020-05-27 09:49:43 -0500

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
         if @user.valid?
             @user.save
+            session[:user_id] = @user.id
             redirect_to @user
         else
             flash[:errors] = @user.errors.full_messages
