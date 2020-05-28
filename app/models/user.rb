9c5e9988b643b8a295c0c05ef6f9c274c427faf2
github username <github email address>
2020-05-28 08:25:07 -0500

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,34 +11,4 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 3}
     validates :username, :password, format: { without: /\s/, message: "cannot have spaces" }
     
-       def self.total_products
-        Product.all.count
-       end
-
-       def self.least_selling_product
-        User.all.min_by{|user| user.product}.name
-       end
-
-       def self.most_selling_product
-         User.all.max_by {|user| user.products.count}.name
-       end
-    
-     
-        def self.total_number_sales_per_order 
-        @order.products.each do |p| 
-            p.name.count
-        end 
-           
-        
-      def self.total_amount_sales_per_order 
-        total = 0 
-        @order.products.each do |product|
-        total += product.cost
-        total
-        end
-       end 
-
-      
-       
-   
 end
